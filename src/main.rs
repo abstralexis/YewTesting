4a79e97b6070ee7babb58b5841ffdcd8c58f4701
AlexisComix <thecomixguy@hotmail.com>
2023-02-06 09:24:53 +0000

    Basic Yew Example Code

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#![allow(non_snake_case)]   // Crate name isn't snake case
+
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
